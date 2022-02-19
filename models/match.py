a392bab8a9318a2579ad0eb420a314edd8aa4def
jmanchuck <jjcheung0000@gmail.com>
2022-02-19 17:36:02 +0000

    match object, unpack response

--- models/match.py
+++ models/match.py
@@ -1,4 +1,69 @@
+from typing import Dict, List, Any
+
 
 class Match:
 
-    pass
+    def __init__(self, metadata: Dict[str, Any], info: Dict[str, Any]):
+        self.metadata = MetadataDto(**metadata)
+        self.info = InfoDto(info)
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__} @ [{self.metadata}], [{self.info}]'
+
+
+class MetadataDto:
+
+    def __init__(self, dataVersion: str, matchId: str, participants: List[str]):
+
+        self.dataVersion = dataVersion
+        self.matchId = matchId
+        self.participants = participants
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__} @ {self.matchId}'
+
+
+class InfoDto:
+
+    def __init__(self, info: Dict[str, Any]):
+        # CUSTOM_GAME, MATCHED_GAME
+        self.gameMode = info['gameMode']
+        self.gameType = info['gameType']
+
+        self.mapId = info['mapId']
+
+        self.participants = [ParticipantDto(
+            participant) for participant in info['participants']]
+        self.teams = [TeamDto(team) for team in info['teams']]
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__} @ {self.gameMode}, {self.gameType}'
+
+
+class ParticipantDto:
+
+    def __init__(self, participantInfo: Dict[str, Any]):
+        self.assists: int = participantInfo['assists']
+        self.championName: str = participantInfo['championName']
+        self.championId: int = participantInfo['championId']
+        self.deaths: int = participantInfo['deaths']
+        self.goldEarned: int = participantInfo['goldEarned']
+        self.kills: int = participantInfo['kills']
+        self.participantId: int = participantInfo['participantId']
+        self.timeCCingOthers: int = participantInfo['timeCCingOthers']
+        self.totalDamageDealtToChampions: int = participantInfo['totalDamageDealtToChampions']
+        self.win: bool = participantInfo['win']
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__} @ {self.participantId}'
+
+
+class TeamDto:
+
+    def __init__(self, teamInfo: Dict[str, Any]):
+        self.kills: int = teamInfo['objectives']['champion']['kills']
+        self.teamId: int = teamInfo['teamId']
+        self.win: bool = teamInfo['win']
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__} @ {self.teamId}'
--- request.py
+++ request.py
@@ -1,34 +1,45 @@
 import json
 import requests
 import os
+from dotenv import load_dotenv
 from typing import Dict, Any, List
-from models.summoner import Summoner
 
+from models.summoner import Summoner
+from models.match import Match
+
+load_dotenv()
 API_KEY = os.getenv("RIOT_API_KEY")
 
-get_user_url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
-get_user_matches_url = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"
-get_match_url = "https://europe.api.riotgames.com/lol/match/v5/matches/"
+GET_USER_URL = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
+GET_USER_MATCHES_URL = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"
+GET_MATCH_URL = "https://europe.api.riotgames.com/lol/match/v5/matches/"
 
 
 def get_summoner(summoner_name: str) -> Summoner:
-    url = get_user_url + summoner_name + f"?api_key={API_KEY}"
+    url = GET_USER_URL + summoner_name + f"?api_key={API_KEY}"
     print(f'Calling: {url}')
     response = requests.get(url).json()
     try:
         summoner = Summoner(**response)
+        return summoner
     except TypeError:
         print('something fucked up')
-    return summoner
+    return None
 
 
 def get_recent_matches(summoner: Summoner, count: int = 20, start: int = 0) -> List[str]:
-    url = f"{get_user_matches_url}{summoner.puuid}/ids?start={start}&count={count}&api_key={API_KEY}"
+    url = f"{GET_USER_MATCHES_URL}{summoner.puuid}/ids?start={start}&count={count}&api_key={API_KEY}"
     response = requests.get(url)
     return response.json()
 
 
 def get_match(match_id: str) -> Dict[str, Any]:
-    url = f"{get_match_url}{match_id}?api_key={API_KEY}"
-    response = requests.get(url)
-    return response.json()
+    url = f"{GET_MATCH_URL}{match_id}?api_key={API_KEY}"
+    response = requests.get(url).json()
+    try:
+        match = Match(**response)
+        return match
+    except TypeError as e:
+        print(f'fcked up {e}')
+        raise
+    return None
