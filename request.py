aaa470f4bbcffc6071b33b68d2ee5116e94dbfb9
jmanchuck <jjcheung0000@gmail.com>
2022-02-19 16:10:29 +0000

    get summoner request

---
+++ request.py
@@ -0,0 +1,15 @@
+import json
+import requests
+import typing
+import os
+from typing import Dict, Any, List
+
+API_KEY = os.getenv("RIOT_API_KEY")
+
+get_user_url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
+
+
+def get_summoner(summoner_name: str, region: str) -> Dict[str, Any]:
+    url = get_user_url + summoner_name + f"?api_key={API_KEY}"
+    response = requests.get(url)
+    return response.json()
