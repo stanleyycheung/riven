3e8514099e15dc696d59c8a8c74a7cb2733dcf93
jmanchuck <jjcheung0000@gmail.com>
2022-02-21 00:01:37 +0000

    add function to plot win rate with summoners

--- analysis.py
+++ analysis.py
@@ -2,10 +2,11 @@ from collections import Counter
 from typing import List
 from models.match import Match
 from models.summoner import Summoner
+import matplotlib.pyplot as plt
 import request
 
 
-def get_aram_matches(summoner: Summoner, num_of_matches = 500) -> List[Match]:
+def get_aram_matches(summoner: Summoner, num_of_matches=500) -> List[Match]:
     match_ids = request.get_recent_matches(summoner, num_of_matches)
     matches = []
     for match_id in match_ids:
@@ -14,15 +15,20 @@ def get_aram_matches(summoner: Summoner, num_of_matches = 500) -> List[Match]:
             matches.append(match)
     return matches
 
+
 def get_most_played_champions(matches: List[Match], summoner: Summoner) -> dict:
-    champion_count = Counter(match.info.participants[summoner.puuid].championName for match in matches)
+    champion_count = Counter(
+        match.info.participants[summoner.puuid].championName for match in matches)
     return champion_count
 
+
 def get_win_rate(matches: List[Match], summoner: Summoner) -> float:
-    win_loss = Counter(match.info.participants[summoner.puuid].win for match in matches)
+    win_loss = Counter(
+        match.info.participants[summoner.puuid].win for match in matches)
     return win_loss[True]/sum(win_loss.values())
 
-def get_most_played_with(summoner: Summoner, other_summoners: List[Summoner], get_win_rate: bool=True):
+
+def get_most_played_with(summoner: Summoner, other_summoners: List[Summoner], get_win_rate: bool = True):
     aram_matches = get_aram_matches(summoner)
     played_with = {summoner.name: 0 for summoner in other_summoners}
     won_games = {summoner.name: 0 for summoner in other_summoners}
@@ -47,3 +53,17 @@ def get_most_played_with(summoner: Summoner, other_summoners: List[Summoner], ge
         return played_with
 
 
+def plot_most_played_with(summoner: Summoner, other_summoners: List[Summoner], get_win_rate: bool = True):
+    played_with = get_most_played_with(summoner, other_summoners, get_win_rate)
+
+    fig = plt.figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+
+    names = played_with.keys()
+    games_played = [x[0] for x in played_with.values()]
+    win_rates = [x[1] * 100 for x in played_with.values()]
+
+    ax.set_ylim(0, 100)
+    ax.bar(names, win_rates)
+
+    plt.show()
