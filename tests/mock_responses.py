81a5e71979a675d8c139182b51fd96312072f163
stanleyycheung <stanleyycheung@gmail.com>
2022-02-27 22:17:00 +0000

    refactor

---
+++ tests/mock_responses.py
@@ -0,0 +1,9 @@
+SUMMONER_MOCK_RESPONSE = {
+    "id": "test_id",
+    "accountId": "test_accountId",
+    "puuid": "test_puuid",
+    "name": "test_summoner",
+    "profileIconId": 0,
+    "revisionDate": 0,
+    "summonerLevel": 0
+}
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,29 +1,20 @@
 
 from unittest.mock import patch
-
 from riven.request import get_summoner
+from mock_responses import SUMMONER_MOCK_RESPONSE
 
 
 class TestRequest:
-
     @patch('riven.request.call_url')
     def test_get_summoner(self, mock_call_url):
-        mock_response ={
-            "id": "test_id",
-            "accountId": "test_accountId",
-            "puuid": "test_puuid",
-            "name": "test_summoner",
-            "profileIconId": 0,
-            "revisionDate": 0,
-            "summonerLevel": 0
-        }
-        mock_call_url.return_value.json.return_value = mock_response
+        mock_call_url.return_value.json.return_value = SUMMONER_MOCK_RESPONSE
         test_summoner = get_summoner('test_summoner')
-        assert test_summoner.summoner_id == mock_response['id']
-        assert test_summoner.accountId == mock_response['accountId']
-        assert test_summoner.puuid == mock_response['puuid']
-        assert test_summoner.name == mock_response['name']
-        assert test_summoner.profileIconId == mock_response['profileIconId']
-        assert test_summoner.summonerLevel == mock_response['summonerLevel']
+        assert test_summoner.summoner_id == SUMMONER_MOCK_RESPONSE['id']
+        assert test_summoner.accountId == SUMMONER_MOCK_RESPONSE['accountId']
+        assert test_summoner.puuid == SUMMONER_MOCK_RESPONSE['puuid']
+        assert test_summoner.name == SUMMONER_MOCK_RESPONSE['name']
+        assert test_summoner.profileIconId == SUMMONER_MOCK_RESPONSE['profileIconId']
+        assert test_summoner.summonerLevel == SUMMONER_MOCK_RESPONSE['summonerLevel']
 
-
+    def get_recent_matches(self):
+        pass
